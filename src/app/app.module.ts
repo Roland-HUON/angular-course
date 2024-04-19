3073da20269d7d60acec80fca03cdfbf5074513e
Roland HUON <huonroland91@gmail.com>
2024-04-19 21:30:28 +0200

    Add button and styles for active button in app.component.css Update app.module.ts to include new components and routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { RouterLink, RouterLinkActive, RouterModule, RouterOutlet, provideRouter } from '@angular/router';
+import { routes } from './app.routes';
 import { AppComponent } from './app.component';
+import { CrisisListComponent } from './crisis-list/crisis-list.component';
+import { HeroesListComponent } from './heroes-list/heroes-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CrisisListComponent,
+    HeroesListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [provideRouter(routes)],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import {Routes, RouterModule} from '@angular/router';
+import { CrisisListComponent } from './crisis-list/crisis-list.component';
+import { HeroesListComponent } from './heroes-list/heroes-list.component';
+
+export const routes : Routes = [
+    {path: 'crisis-list', component: CrisisListComponent},
+    {path: 'heroes-list', component: HeroesListComponent},
+    {path: '', redirectTo: '/heroes-list', pathMatch: 'full'},
+];
